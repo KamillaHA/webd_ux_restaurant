36bb314f8305d4bf28e6d84571c13619c495f27d
Kamilla Huhnke <kamillahuhnke@me.com>
2025-04-02 13:50:33 +0200

    remove SPA

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // Single Page Application (SPA)
 
-'use strict';
+// 'use strict';
 
-document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
-    menuOption.addEventListener('click', function() {
-        // The clicked menu option is selected.
-        document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
-            option.classList.remove('selected');
-        });
-        this.classList.add('selected');
+// document.querySelectorAll('nav > ul > li > a').forEach(menuOption => {
+//     menuOption.addEventListener('click', function() {
+//         // The clicked menu option is selected.
+//         document.querySelectorAll(`nav > ul > li > a`).forEach(option => {
+//             option.classList.remove('selected');
+//         });
+//         this.classList.add('selected');
         
-        // The page title gets updated 
-        document.title = `${this.innerText} | Restaurant KEA`;
+//         // The page title gets updated 
+//         document.title = `${this.innerText} | Restaurant KEA`;
         
-        // The clicked page is shown and the other pages are hidden.
-        const dataTarget = this.getAttribute('data-target');        
-        document.querySelector(`#${dataTarget}`).classList.remove('hidden');
-        document.querySelectorAll(`.page:not(#${dataTarget})`).forEach(page => {
-            page.classList.add('hidden');
-        });
-    });
-});
+//         // The clicked page is shown and the other pages are hidden.
+//         const dataTarget = this.getAttribute('data-target');        
+//         document.querySelector(`#${dataTarget}`).classList.remove('hidden');
+//         document.querySelectorAll(`.page:not(#${dataTarget})`).forEach(page => {
+//             page.classList.add('hidden');
+//         });
+//     });
+// });
